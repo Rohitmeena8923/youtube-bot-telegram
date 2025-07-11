dc8f1b3e4993ad4852ea1fd5a0134f1e4ec36404
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-07-11 19:40:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 
 import os
 
-    API_ID = int(os.environ.get("26209173"))
-    API_HASH = os.environ.get("a4d86637bb1b8db35e92196493798279")
-    BOT_TOKEN = os.environ.get("6700029902:AAGRtGmE47BhVprmXmVjyc8lmu9RLiWtmJ4")
+    API_ID = int(os.environ.get("27775431"))
+    API_HASH = os.environ.get("b70bb1d45a1d05236671d4cc615e40f9")
+    BOT_TOKEN = os.environ.get("")
